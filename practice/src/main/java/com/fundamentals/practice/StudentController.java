bcc573b78a00c7cb5bec9a10891cfc3144c59683
mosafari <m.safari2680@gmail.com>
2023-10-01 18:30:31 +0330

    Get method + query params

--- practice/src/main/java/com/fundamentals/practice/StudentController.java
+++ practice/src/main/java/com/fundamentals/practice/StudentController.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.springlesson.beans.Student;
@@ -27,6 +28,11 @@ public class StudentController {
 	
 	@GetMapping("/student/{firstName}/{lastName}")
 	public Student studentPatthVariable(@PathVariable("firstName") String firstName, @PathVariable("lastName") String lastName) {
+		return new Student(firstName, lastName);
+	}
+	
+	@GetMapping("/student/query")
+	public Student studentQueryParam(@RequestParam(name="firstName") String firstName, @RequestParam(name="lastName") String lastName) {
 		return new Student(firstName, lastName);
 	}
 
