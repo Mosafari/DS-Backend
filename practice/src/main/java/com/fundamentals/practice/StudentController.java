b4714eec488bfcf9c53108b9226d2f447032962f
mosafari <m.safari2680@gmail.com>
2023-10-02 12:58:36 +0330

    Post method

--- practice/src/main/java/com/fundamentals/practice/StudentController.java
+++ practice/src/main/java/com/fundamentals/practice/StudentController.java
@@ -5,6 +5,8 @@ import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -34,6 +36,11 @@ public class StudentController {
 	@GetMapping("/student/query")
 	public Student studentQueryParam(@RequestParam(name="firstName") String firstName, @RequestParam(name="lastName") String lastName) {
 		return new Student(firstName, lastName);
+	}
+	
+	@PostMapping("/student")
+	public void addStudent(@RequestBody Student student) {
+		students.add(student);
 	}
 
 }
